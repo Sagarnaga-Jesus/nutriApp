81211c35347ba69832a0b69149822c24751da830
Sagarnaga-Jesus <23308060610335@cetis61.edu.mx>
2025-11-02 20:15:25 -0700

    por ahora eso

--- app.py
+++ app.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("base.html")
+    return render_template("home.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/registro')
+def registro():
+    return render_template("registro.html")
+
+@app.route('/perfil')
+def perfil():
+    return render_template("perfil.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
