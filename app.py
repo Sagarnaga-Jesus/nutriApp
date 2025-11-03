d35e05ad6fc6c6c545f2071845b5c7b367e4fceb
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2025-11-02 17:59:54 -0700

    avances_base

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
-
+@app.route('/')
+def index():
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
