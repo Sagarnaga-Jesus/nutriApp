638d9f86b8188900c3db100505440d15df9ce0f6
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2025-11-24 09:09:09 -0500

    solo agregue from jejejejeje

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from datetime import timedelta,datetime
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash
+import re 
 import requests
 
 
